dd7fe034711162030065846ba1a658d426961691
Hoseinkhan1998 <hosein.mazinani.1998@gmail.com>
2025-06-15 11:19:09 +0330

    add transition for carts

--- src/components/Icons.vue
+++ src/components/Icons.vue
@@ -25,36 +25,16 @@ const chats = ref([
     date: '۱۴۰۴/۰۲/۱۰',
   },
 ])
-const tasks = ref([
-  {
-    id: 1,
-    title: 'تامین کالا و تجهیزات',
-    day: '۳',
-  },
-  {
-    id: 2,
-    title: 'نصب رابط گرافیکی جدید پرتال',
-    day: '۵',
-  },
-  {
-    id: 3,
-    title: 'باز طراحی Ui پرتال',
-    day: '۸',
-  },
-])
 </script>
 
 <template>
   <div class="grid grid-cols-12 gap-10">
     <!-- task cart -->
     <div
-      class="col-span-4 group relative overflow-hidden text-neutral-900 h-[30vh] bg-neutral-100 rounded-xl cursor-pointer shadow-lg shadow-neutral-300"
+      class="col-span-4 text-neutral-900 h-[30vh] bg-neutral-100 rounded-xl cursor-pointer shadow-lg shadow-neutral-300"
     >
-      <!-- default show -->
-      <div class="absolute inset-0 flex flex-col justify-center items-center gap-5">
-        <div
-          class="flex w-[20vh] h-[20vh] relative transition-all duration-300 group-hover:w-[8vh] group-hover:h-[8vh] group-hover:opacity-0 group-hover:translate-x-20 group-hover:translate-y-5 z-10"
-        >
+      <div class="flex gap-5 justify-center items-center h-full flex-col">
+        <div class="flex w-[20vh] h-[20vh] relative">
           <svg
             class="w-full h-full"
             viewBox="0 0 150 150"
@@ -116,135 +96,7 @@ const tasks = ref([
             class="left-[0.7vh] top-[0.7vh] absolute border-solid border-neutral-100 bg-red-600 w-[15%] h-[15%] rounded-full hidden"
           ></div>
         </div>
-        <p
-          class="font-semibold opacity-100 group-hover:opacity-0 duration-300 group-hover:-translate-y-32 translate-y-0 transition-all"
-        >
-          شما ۳ وظیفه با تاخیر دارید
-        </p>
-      </div>
-      <!-- hover show -->
-      <div
-        class="absolute inset-0 flex flex-col p-4 opacity-0 group-hover:opacity-100 pointer-events-none group-hover:pointer-events-auto transition-opacity duration-700 z-20"
-      >
-        <!-- tasks -->
-        <div class="flex justify-center items-center gap-1">
-          <div class="font-semibold text-[#75781B]">
-            <svg
-              width="12"
-              height="14"
-              viewBox="0 0 12 14"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path d="M12 0L0 7L12 14V0Z" fill="#75781B" />
-            </svg>
-          </div>
-          <p class="text-[28px] font-semibold text-[#75781B]">وظایف من</p>
-          <div class="font-semibold text-[#75781B]">
-            <svg
-              width="12"
-              height="14"
-              viewBox="0 0 12 14"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path d="M0 0L12 7L0 14V0Z" fill="#75781B" />
-            </svg>
-          </div>
-        </div>
-        <!-- title -->
-        <p
-          class="font-semibold text-center text-[10px] opacity-0 translate-y-7 scale-100 transition-transform duration-150 group-hover:opacity-100 group-hover:translate-y-0 group-hover:animate-pop-up-once delay-150"
-        >
-          شما ۳ وظیفه با تاخیر دارید
-        </p>
-        <div class="!flex items-center gap-3 mt-3">
-          <!-- icon -->
-          <div
-            class="h-[8vh] w-[8vh] transition-all -translate-y-5 group-hover:translate-y-0 -translate-x-10 group-hover:translate-x-0 duration-300 opacity-0 group-hover:opacity-100 group-hover:delay-100"
-          >
-            <svg
-              class="w-full h-full"
-              viewBox="0 0 70 70"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M32.0834 56.875H61.25"
-                stroke="#75781B"
-                stroke-width="5"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-              />
-              <path
-                d="M58.3334 36.4583H61.25"
-                stroke="#75781B"
-                stroke-width="5"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-              />
-              <path
-                d="M32.0834 36.4583H48.0959"
-                stroke="#75781B"
-                stroke-width="5"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-              />
-              <path
-                d="M32.0834 16.0417H61.25"
-                stroke="#75781B"
-                stroke-width="5"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-              />
-              <path
-                d="M8.75 16.0417L11.6667 18.9583L20.4167 10.2083"
-                stroke="#75781B"
-                stroke-width="5"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-              />
-              <path
-                d="M8.75 36.4583L11.6667 39.375L20.4167 30.625"
-                stroke="#75781B"
-                stroke-width="5"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-              />
-              <path
-                d="M8.75 56.875L11.6667 59.7917L20.4167 51.0417"
-                stroke="#75781B"
-                stroke-width="5"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-              />
-            </svg>
-          </div>
-          <!-- chats -->
-          <div
-            class="!flex flex-col transition-opacity duration-700 opacity-0 group-hover:opacity-100"
-          >
-            <div
-              v-for="task in tasks"
-              :key="task.id"
-              class="grid grid-cols-12 gap-1 border-b-2 py-1 border-red-600 border-solid items-center"
-            >
-              <div class="col-span-2">
-                <img src="/images/Ellipse9.png" class="h-[5vh] w-[5vh] rounded-full" />
-              </div>
-              <div class="col-span-7">
-                <p class="text-xs w-44 font-semibold line-clamp-2">
-                  {{ task.title }}
-                </p>
-              </div>
-              <div class="col-span-3">
-                <p class="text-red-600 text-xs text-center font-semibold">
-                  {{ task.day }} روز تاخیر
-                </p>
-              </div>
-            </div>
-          </div>
-        </div>
+        <p class="font-semibold">شما 0 وظیفه با تاخیر دارید</p>
       </div>
     </div>
     <!-- Chat cart -->
@@ -400,13 +252,10 @@ const tasks = ref([
     </div>
     <!-- Process cart -->
     <div
-      class="col-span-4 group relative overflow-hidden text-neutral-900 h-[30vh] bg-neutral-100 rounded-xl cursor-pointer shadow-lg shadow-neutral-300"
+      class="col-span-4 text-neutral-900 h-[30vh] bg-neutral-100 rounded-xl cursor-pointer shadow-lg shadow-neutral-300"
     >
-      <!-- default show -->
-      <div class="absolute inset-0 flex flex-col justify-center items-center gap-5">
-        <div
-          class="flex w-[20vh] h-[20vh] relative transition-all duration-300 group-hover:w-[8vh] group-hover:h-[8vh] group-hover:opacity-0 group-hover:translate-x-20 group-hover:translate-y-5 z-10"
-        >
+      <div class="flex gap-5 justify-center h-full items-center flex-col">
+        <div class="flex w-[20vh] h-[20vh] relative">
           <svg
             class="w-full h-full"
             viewBox="0 0 150 150"
@@ -442,115 +291,13 @@ const tasks = ref([
               stroke-linejoin="round"
             />
           </svg>
+          <!-- badg -->
           <div
             style="border-width: 0.2rem"
             class="left-[0.7vh] top-[0.7vh] absolute border-solid border-neutral-100 bg-red-600 w-[15%] h-[15%] rounded-full"
           ></div>
         </div>
-        <p
-          class="font-semibold opacity-100 group-hover:opacity-0 duration-300 group-hover:-translate-y-32 translate-y-0 transition-all"
-        >
-          شما ۳ فرآیند برای اقدام دارید
-        </p>
-      </div>
-      <!-- hover show -->
-      <div
-        class="absolute inset-0 flex flex-col p-4 opacity-0 group-hover:opacity-100 pointer-events-none group-hover:pointer-events-auto transition-opacity duration-700 z-20"
-      >
-        <!-- گفتگو -->
-        <div class="flex justify-center items-center gap-1">
-          <div class="font-semibold text-[#75781B]">
-            <svg
-              width="12"
-              height="14"
-              viewBox="0 0 12 14"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path d="M12 0L0 7L12 14V0Z" fill="#75781B" />
-            </svg>
-          </div>
-          <p class="text-[28px] font-semibold text-[#75781B]">گفتگو</p>
-          <div class="font-semibold text-[#75781B]">
-            <svg
-              width="12"
-              height="14"
-              viewBox="0 0 12 14"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path d="M0 0L12 7L0 14V0Z" fill="#75781B" />
-            </svg>
-          </div>
-        </div>
-        <!-- title -->
-        <p
-          class="font-semibold text-center text-[10px] opacity-0 translate-y-7 scale-100 transition-transform duration-150 group-hover:opacity-100 group-hover:translate-y-0 group-hover:animate-pop-up-once delay-150"
-        >
-          شما 3 گفتگوی مشاهده نشده دارین
-        </p>
-        <div class="!flex items-center gap-3 mt-3">
-          <!-- icon -->
-          <div
-            class="h-[8vh] w-[8vh] transition-all -translate-y-5 group-hover:translate-y-0 -translate-x-10 group-hover:translate-x-0 duration-300 opacity-0 group-hover:opacity-100 group-hover:delay-100"
-          >
-            <svg
-              class="h-full w-full"
-              viewBox="0 0 65 65"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                fill-rule="evenodd"
-                clip-rule="evenodd"
-                d="M41.1957 33.7218C41.1957 32.0879 42.5202 30.7634 44.154 30.7634H44.1806C45.8145 30.7634 47.139 32.0879 47.139 33.7218C47.139 35.3556 45.8145 36.6801 44.1806 36.6801H44.154C42.5202 36.6801 41.1957 35.3556 41.1957 33.7218Z"
-                fill="#75781B"
-              />
-              <path
-                fill-rule="evenodd"
-                clip-rule="evenodd"
-                d="M29.3358 33.7218C29.3358 32.0879 30.6602 30.7634 32.2941 30.7634H32.3207C33.9546 30.7634 35.279 32.0879 35.279 33.7218C35.279 35.3556 33.9546 36.6801 32.3207 36.6801H32.2941C30.6602 36.6801 29.3358 35.3556 29.3358 33.7218Z"
-                fill="#75781B"
-              />
-              <path
-                fill-rule="evenodd"
-                clip-rule="evenodd"
-                d="M17.4758 33.7218C17.4758 32.0879 18.8003 30.7634 20.4341 30.7634H20.4607C22.0946 30.7634 23.4191 32.0879 23.4191 33.7218C23.4191 35.3556 22.0946 36.6801 20.4607 36.6801H20.4341C18.8003 36.6801 17.4758 35.3556 17.4758 33.7218Z"
-                fill="#75781B"
-              />
-              <path
-                fill-rule="evenodd"
-                clip-rule="evenodd"
-                d="M51.8493 13.1446C41.1746 2.46571 23.8291 2.46571 13.1544 13.1446L13.1539 13.1451C4.79399 21.5034 2.9785 33.8932 7.67058 44.0327C7.68634 44.0668 7.70123 44.1012 7.71524 44.136C7.98791 44.8134 7.98178 45.5812 7.96951 46.0154C7.95409 46.5606 7.89378 47.1888 7.81934 47.8278C7.74896 48.4319 7.65956 49.0975 7.5687 49.7741L7.54981 49.9147C7.4514 50.6479 7.35119 51.4005 7.26373 52.1543C7.08636 53.683 6.97496 55.1105 7.02452 56.2525C7.04921 56.8216 7.11144 57.2433 7.19087 57.5323C7.21763 57.6296 7.24214 57.6966 7.26022 57.7398C7.30343 57.7579 7.37047 57.7824 7.46785 57.8091C7.75697 57.8885 8.17873 57.9506 8.74782 57.9753C9.88982 58.0247 11.3173 57.9134 12.8459 57.7362C13.5995 57.6488 14.3521 57.5487 15.0852 57.4504L15.2249 57.4316C15.9017 57.3408 16.5676 57.2515 17.1719 57.1812C17.8109 57.1068 18.4389 57.0466 18.9841 57.0313C19.4183 57.0191 20.1859 57.0131 20.8632 57.2858C20.8977 57.2997 20.932 57.3145 20.9658 57.3302C31.1075 62.0205 43.4925 60.2071 51.8492 51.8465L51.8495 51.8462C62.5257 41.1686 62.5468 23.8409 51.8497 13.145L51.8493 13.1446ZM54.9873 10.007C42.5794 -2.40523 22.4236 -2.40509 10.016 10.0074C0.359848 19.6621 -1.77386 33.9343 3.5344 45.6583C3.53599 45.7101 3.53672 45.786 3.53378 45.89C3.524 46.2359 3.48219 46.7088 3.41165 47.3143C3.34597 47.8781 3.26155 48.5067 3.16896 49.1962L3.15175 49.3244C3.05357 50.0559 2.9484 50.8448 2.85581 51.6428C2.67308 53.2176 2.52564 54.9344 2.59119 56.4449C2.62405 57.2021 2.71287 57.9837 2.91205 58.7083C3.10669 59.4165 3.451 60.2341 4.10879 60.8919L4.10981 60.893C4.76778 61.5502 5.58529 61.894 6.2931 62.0883C7.01752 62.2872 7.79886 62.3759 8.55583 62.4086C10.0659 62.474 11.7824 62.3267 13.3569 62.1441C14.1548 62.0516 14.9436 61.9466 15.675 61.8485L15.802 61.8315C16.4918 61.7389 17.1208 61.6545 17.6848 61.5889C18.2901 61.5185 18.763 61.4768 19.1087 61.4671C19.2129 61.4641 19.2889 61.4649 19.3407 61.4665C31.0657 66.7724 45.3336 64.6419 54.9875 54.9838C67.3938 42.5758 67.4202 22.4384 54.9873 10.007Z"
-                fill="#75781B"
-              />
-            </svg>
-          </div>
-          <!-- chats -->
-          <div
-            class="!flex flex-col transition-opacity duration-700 opacity-0 group-hover:opacity-100"
-          >
-            <div
-              v-for="chat in chats"
-              :key="chat.id"
-              class="grid grid-cols-12 gap-1 border-b-2 py-1 border-neutral-400 border-solid"
-            >
-              <div class="col-span-2">
-                <img src="/images/Ellipse9.png" class="h-[5vh] w-[5vh] rounded-full" />
-              </div>
-              <div class="col-span-7 !flex flex-col">
-                <p class="text-sm font-semibold truncate">{{ chat.user }}</p>
-                <p class="text-[9px] w-44 text-neutral-600 font-semibold line-clamp-2">
-                  {{ chat.message }}
-                </p>
-              </div>
-              <div class="col-span-3 !flex flex-col gap-1 mt-1">
-                <p class="text-neutral-600 text-[9px] text-center font-semibold">{{ chat.date }}</p>
-                <p class="text-neutral-600 text-[9px] text-center font-semibold">{{ chat.time }}</p>
-              </div>
-            </div>
-          </div>
-        </div>
+        <p class="font-semibold">شما 3 فرآیند برای اقدام دارید</p>
       </div>
     </div>
   </div>
