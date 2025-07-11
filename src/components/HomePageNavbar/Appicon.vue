11ce663c3b9086b7a016b0192006afe20ff77ff4
Hoseinkhan1998 <hosein.mazinani.1998@gmail.com>
2025-07-11 23:31:58 +0330

    add

--- src/components/HomePageNavbar/Appicon.vue
+++ src/components/HomePageNavbar/Appicon.vue
@@ -1,24 +1,40 @@
 <script setup></script>
 <template>
-  <div>
-    <svg class="size-7 cursor-pointer" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <g clip-path="url(#clip0_1_4783)">
-        <path d="M14 6C14 7.10457 14.8954 8 16 8C17.1046 8 18 7.10457 18 6C18 4.89543 17.1046 4 16 4C14.8954 4 14 4.89543 14 6Z" fill="white" />
-        <path d="M4 6C4 7.10457 4.89543 8 6 8C7.10457 8 8 7.10457 8 6C8 4.89543 7.10457 4 6 4C4.89543 4 4 4.89543 4 6Z" fill="white" />
-        <path d="M24 6C24 7.10457 24.8954 8 26 8C27.1046 8 28 7.10457 28 6C28 4.89543 27.1046 4 26 4C24.8954 4 24 4.89543 24 6Z" fill="white" />
-        <path d="M14 16C14 17.1046 14.8954 18 16 18C17.1046 18 18 17.1046 18 16C18 14.8954 17.1046 14 16 14C14.8954 14 14 14.8954 14 16Z" fill="white" />
-        <path d="M4 16C4 17.1046 4.89543 18 6 18C7.10457 18 8 17.1046 8 16C8 14.8954 7.10457 14 6 14C4.89543 14 4 14.8954 4 16Z" fill="white" />
-        <path d="M24 16C24 17.1046 24.8954 18 26 18C27.1046 18 28 17.1046 28 16C28 14.8954 27.1046 14 26 14C24.8954 14 24 14.8954 24 16Z" fill="white" />
-        <path d="M14 26C14 27.1046 14.8954 28 16 28C17.1046 28 18 27.1046 18 26C18 24.8954 17.1046 24 16 24C14.8954 24 14 24.8954 14 26Z" fill="white" />
-        <path d="M4 26C4 27.1046 4.89543 28 6 28C7.10457 28 8 27.1046 8 26C8 24.8954 7.10457 24 6 24C4.89543 24 4 24.8954 4 26Z" fill="white" />
-        <path d="M24 26C24 27.1046 24.8954 28 26 28C27.1046 28 28 27.1046 28 26C28 24.8954 27.1046 24 26 24C24.8954 24 24 24.8954 24 26Z" fill="white" />
-      </g>
-      <defs>
-        <clipPath id="clip0_1_4783">
-          <rect width="32" height="32" fill="white" />
-        </clipPath>
-      </defs>
-    </svg>
+  <div class="dropdown dropdown-bottom dropdown-end">
+    <div tabindex="0" role="button" class="">
+      <svg class="size-7 cursor-pointer" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <g clip-path="url(#clip0_1_4783)">
+          <path d="M14 6C14 7.10457 14.8954 8 16 8C17.1046 8 18 7.10457 18 6C18 4.89543 17.1046 4 16 4C14.8954 4 14 4.89543 14 6Z" fill="white" />
+          <path d="M4 6C4 7.10457 4.89543 8 6 8C7.10457 8 8 7.10457 8 6C8 4.89543 7.10457 4 6 4C4.89543 4 4 4.89543 4 6Z" fill="white" />
+          <path d="M24 6C24 7.10457 24.8954 8 26 8C27.1046 8 28 7.10457 28 6C28 4.89543 27.1046 4 26 4C24.8954 4 24 4.89543 24 6Z" fill="white" />
+          <path d="M14 16C14 17.1046 14.8954 18 16 18C17.1046 18 18 17.1046 18 16C18 14.8954 17.1046 14 16 14C14.8954 14 14 14.8954 14 16Z" fill="white" />
+          <path d="M4 16C4 17.1046 4.89543 18 6 18C7.10457 18 8 17.1046 8 16C8 14.8954 7.10457 14 6 14C4.89543 14 4 14.8954 4 16Z" fill="white" />
+          <path d="M24 16C24 17.1046 24.8954 18 26 18C27.1046 18 28 17.1046 28 16C28 14.8954 27.1046 14 26 14C24.8954 14 24 14.8954 24 16Z" fill="white" />
+          <path d="M14 26C14 27.1046 14.8954 28 16 28C17.1046 28 18 27.1046 18 26C18 24.8954 17.1046 24 16 24C14.8954 24 14 24.8954 14 26Z" fill="white" />
+          <path d="M4 26C4 27.1046 4.89543 28 6 28C7.10457 28 8 27.1046 8 26C8 24.8954 7.10457 24 6 24C4.89543 24 4 24.8954 4 26Z" fill="white" />
+          <path d="M24 26C24 27.1046 24.8954 28 26 28C27.1046 28 28 27.1046 28 26C28 24.8954 27.1046 24 26 24C24.8954 24 24 24.8954 24 26Z" fill="white" />
+        </g>
+        <defs>
+          <clipPath id="clip0_1_4783">
+            <rect width="32" height="32" fill="white" />
+          </clipPath>
+        </defs>
+      </svg>
+    </div>
+    <ul tabindex="0" class="dropdown-content text-primarymain flex flex-col w-[35vh] mt-5 rounded-3xl z-30 p-2 -me-14 bg-tintone">
+      <!-- close button -->
+      <div class="flex justify-end">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+          <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
+        </svg>        
+      </div>
+      <!-- profile pic -->
+      <div class=" flex items-center justify-center w-full flex-col gap-2 mt-3">
+        <img src="/images/chat2.jpg" class=" w-[102px] h-[102px] rounded-full" alt="">
+        <p class=" font-semibold text-lg">هادی احدی</p>
+        <p class=" text-sm">مدیر اداره برنامه ریزی</p>
+      </div>
+    </ul>
   </div>
 </template>
 
